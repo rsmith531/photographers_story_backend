d031a191ac3cef9ae1e48e8b587bbf7fccd5e55b
rsmith531 <2ryan.d.smith+github@gmail.com>
2025-08-27 23:35:15 -0400

    write test for health check endpoint

---
+++ Tests/HealthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tests;
+
+public class HealthControllerTests
+{
+    [Fact]
+    public void HealthCheck_Returns204_ForGetRequest()
+    {
+        // Arrange
+        var controller = new Api.Controllers.HealthController();
+        
+        // Act
+        var result = controller.Get();
+
+        // Assert
+        var noContentResult = Assert.IsType<NoContentResult>(result);
+        Assert.Equal(204, noContentResult.StatusCode);
+
+    }
+}
