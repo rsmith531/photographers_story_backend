5c848f44fef2a1afa71369d2e36cc88dfd6c9a71
rsmith531 <2ryan.d.smith+github@gmail.com>
2025-08-27 23:43:47 -0400

    cache openAPI doc in prod

--- Api/Program.cs
+++ Api/Program.cs
@@ -2,6 +2,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// create a cache for the generated OpenAPI document
+builder.Services.AddOutputCache(options =>
+{
+    options.AddBasePolicy(policy => policy.Expire(TimeSpan.FromMinutes(10)));
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -9,6 +15,11 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsProduction())
+{
+    // cache the generated OpenAPI document in prod
+    app.MapOpenApi().CacheOutput();
+}
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
