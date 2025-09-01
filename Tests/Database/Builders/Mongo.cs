19f001c0356aa04c2d775ddf81d7ebbfabdd540a
rsmith531 <2ryan.d.smith+github@gmail.com>
2025-09-01 15:35:34 -0400

    write a test builder for the Mongo Post object

---
+++ Tests/Database/Builders/Mongo.cs
@@ -0,0 +1,63 @@
+// Tests/Database/Builders/Mongo.cs
+
+using Database.Mongo.Models;
+using MongoDB.Bson;
+
+namespace Tests.Builders;
+
+public class PostBuilder
+{
+    private readonly Post _post;
+
+    public PostBuilder()
+    {
+        _post = new Post
+        {
+            Id = ObjectId.GenerateNewId().ToString(),
+            Title = "Default Test Post Title",
+            Slug = "default-test-post-slug",
+            ArticleContent = "This is the default post body.",
+            Author = "Test Person",
+            PublishedAt = null,
+            Tags = ["photography", "test"],
+            CreatedAt = DateTime.UtcNow,
+            Location = new Location
+            {
+                Id = "1",
+                Place = "Test City",
+                Latitude = 41.1561268114726,
+                Longitude = -81.41418385982185
+            },
+            Summary = "A post for testing purposes"
+        };
+    }
+
+    public PostBuilder WithTitle(string title)
+    {
+        _post.Title = title;
+        return this;
+    }
+
+    public PostBuilder WithSlug(string slug)
+    {
+        _post.Slug = slug;
+        return this;
+    }
+
+    public PostBuilder IsPublished()
+    {
+        _post.PublishedAt = DateTime.UtcNow;
+        return this;
+    }
+
+    public PostBuilder IsDraft()
+    {
+        _post.PublishedAt = null;
+        return this;
+    }
+
+    public Post Build()
+    {
+        return _post;
+    }
+}
