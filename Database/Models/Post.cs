2e1fb679da43684ee014028f429810745c5ee112
rsmith531 <2ryan.d.smith+github@gmail.com>
2025-09-01 18:27:53 -0400

    add DTO for a Post object

--- Database/Models/Post.cs
+++ Database/Models/Post.cs
@@ -13,10 +13,23 @@ public class Post
     public Photo? CoverPhoto { get; set; }
     public List<Photo> Photos { get; set; } = [];
     public required string ArticleContent { get; set; } // as markdown
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public DateTime? EditedAt { get; set; }
-    public DateTime? PublishedAt { get; set; } // null value indicates the post has not yet been published
-    public uint ViewCount { get; set; } = 0;
+    public DateTime CreatedAt { get; } = DateTime.UtcNow;
+    public DateTime? EditedAt { get; set; } = null;
+    public DateTime? PublishedAt { get; set; } = null; // null value indicates the post has not yet been published
+    public uint ViewCount { get; } = 0;
     public required Location Location { get; set; }
-    public required uint ReadTimeMinutes { get; set; } = 0;
+    public required uint ReadTimeMinutes { get; set; }
+}
+
+public class PostDTO
+{
+    public List<string> Tags { get; set; } = [];
+    public required string Author { get; set; }
+    public required string Title { get; set; }
+    public required string Summary { get; set; }
+    public Photo? CoverPhoto { get; set; }
+    public List<Photo> Photos { get; set; } = [];
+    public required string ArticleContent { get; set; }
+    public required bool PublishedAt { get; set; }
+    public required Location Location { get; set; }
 }
